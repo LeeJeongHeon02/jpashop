9af437daa7dca84e4b4d1cbc8df0656939788971
이정헌 <aoc05230@naver.com>
2025-08-23 03:55:21 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class Hello {
+
+    private String name;
+    private int age;
+
+
+}
